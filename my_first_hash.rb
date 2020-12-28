c38b2d4fb917fa8619d1e436ec7ab8bac9b5f03f
Luis Martinez <luis_mmartinez@live.com>
2020-12-28 20:17:19 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -2,7 +2,7 @@ def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   my_hash= {
     "name" => "Amigo",
-    "age" => "14"
+    "age" => 14
   }
 end
 
