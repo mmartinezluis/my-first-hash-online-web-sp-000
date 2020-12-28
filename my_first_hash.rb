4f530b901d9117cd080f79ec03c67e5dc1739581
Luis Martinez <luis_mmartinez@live.com>
2020-12-28 20:13:42 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -35,7 +35,7 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-  shipping_maifest["muskets"]= 2
+  shipping_maifest["muskets"] = 2
 
 
   # add 4 gun powder to the shipping_manifest hash below
